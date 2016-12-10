211c777fbf67a0fbc57bfc646fdd8eec207909c4
byackee <vincent.fresnel@gmail.com>
2016-12-10 19:49:20 +0100

    add install python et pip

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Use only if you need to perform changes on the user system such as installing software
+sudo apt-get install -y python python-pip
 sudo pip install rxv 
 sudo pip install simplejson
